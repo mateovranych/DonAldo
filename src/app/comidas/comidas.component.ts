63a1b13d1e719b316d654da5fe2b488f5b4827cd
Matiu <mateovranych@hotmail.com>
2024-05-22 20:04:03 -0300

    comidas y informacion

---
+++ src/app/comidas/comidas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comidas',
+  standalone: true,
+  imports: [],
+  templateUrl: './comidas.component.html',
+  styleUrl: './comidas.component.css'
+})
+export class ComidasComponent {
+
+}
